0e79ff6760dc36117a371fbe3abd1ebe1b1442e4
Suhas-Borkar <suhasborkar80@gmail.com>
2023-01-28 18:48:47 +0530

    create new class student and text

--- Virtual_Base_Class_in_C++_example.cpp
+++ Virtual_Base_Class_in_C++_example.cpp
@@ -1,1 +1,31 @@
 #include <iostream>
+using namespace std;
+
+class student{
+    protected:
+    int roll_no;
+    public:
+    void set_roll(int r){
+        roll_no=r;
+    };
+    void get_roll(){
+        cout<<"The roll no is "<<roll_no;
+    };
+};
+
+class test : virtual public student {
+    protected:
+    float math ,hindi;
+    public:
+    void set_test(float m1,float m2){
+        math = m1;
+        hindi = m2;
+    };
+
+    void get_test(){
+        cout<<"The Marks is"<<endl
+        <<"Math"<<math<<endl
+        <<"hindi"<<hindi<<endl;
+    }
+};
+
