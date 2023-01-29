3806005c8e9b6096949ce19e6de6572dd2320952
Suhas-Borkar <suhasborkar80@gmail.com>
2023-01-29 23:48:42 +0530

    second

---
+++ Initialization_list_in_Constructors_in_Cpp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class test{
+    int a; // here the a is initialized first 
+    int b; // here the b is initialized second
+    public:
+    // test(int x, int y): a(x),b(y)
+    // test(int x, int y): a(x),b(y+x)
+    // test(int x, int y): a(x+y),b(y)
+    // test(int x, int y): a(x),b(y+a)
+    // test(int x, int y): a(x),b(a * y)
+    // test(int x, int y): a(x +y),b(y)
+    // test(int x, int y): a(x+x),b(y)
+    // test(int x, int y): a(x+b),b(y) // this will be create problem becuase of a will be initialized first 
+    // test(int x, int y): a(x)
+    test(int x, int y)
+    {
+        // b = y;
+        a=x;
+        b=y;
+        cout<<"Counstructor executed"<<endl;
+        cout<<"The value of a is "<<a<<endl;
+        cout<<" value of b is "<<b<<endl;
+    }
+};
+
+int main(){
+    test(10 ,12);
+}
